7d4d89b9df73d346930376c6ff588b4e72db766c
Victor Galan <victor.galan@liferay.com>
2019-11-12 10:32:45 +0100

    working project

---
+++ appointments-impl/src/main/java/com/liferay/appointments/internal/resource/v1_0/BaseAppointmentResourceImpl.java
@@ -0,0 +1,88 @@
+package com.liferay.appointments.internal.resource.v1_0;
+
+import com.liferay.appointments.dto.v1_0.Appointment;
+import com.liferay.appointments.resource.v1_0.AppointmentResource;
+import com.liferay.petra.function.UnsafeFunction;
+import com.liferay.portal.kernel.model.Company;
+import com.liferay.portal.vulcan.accept.language.AcceptLanguage;
+import com.liferay.portal.vulcan.pagination.Page;
+import com.liferay.portal.vulcan.util.TransformUtil;
+
+import io.swagger.v3.oas.annotations.tags.Tags;
+
+import java.util.Collection;
+import java.util.Collections;
+import java.util.List;
+
+import javax.annotation.Generated;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.Context;
+import javax.ws.rs.core.UriInfo;
+
+/**
+ * @author Javier Gamarra
+ * @generated
+ */
+@Generated("")
+@Path("/v1.0")
+public abstract class BaseAppointmentResourceImpl
+	implements AppointmentResource {
+
+	@Override
+	@GET
+	@Path("/appointments")
+	@Produces({"application/json", "application/xml"})
+	@Tags(value = {})
+	public Page<Appointment> getAppointmentsPage() throws Exception {
+		return Page.of(Collections.emptyList());
+	}
+
+	public void setContextCompany(Company contextCompany) {
+		this.contextCompany = contextCompany;
+	}
+
+	protected void preparePatch(
+		Appointment appointment, Appointment existingAppointment) {
+	}
+
+	protected <T, R> List<R> transform(
+		Collection<T> collection,
+		UnsafeFunction<T, R, Exception> unsafeFunction) {
+
+		return TransformUtil.transform(collection, unsafeFunction);
+	}
+
+	protected <T, R> R[] transform(
+		T[] array, UnsafeFunction<T, R, Exception> unsafeFunction,
+		Class<?> clazz) {
+
+		return TransformUtil.transform(array, unsafeFunction, clazz);
+	}
+
+	protected <T, R> R[] transformToArray(
+		Collection<T> collection,
+		UnsafeFunction<T, R, Exception> unsafeFunction, Class<?> clazz) {
+
+		return TransformUtil.transformToArray(
+			collection, unsafeFunction, clazz);
+	}
+
+	protected <T, R> List<R> transformToList(
+		T[] array, UnsafeFunction<T, R, Exception> unsafeFunction) {
+
+		return TransformUtil.transformToList(array, unsafeFunction);
+	}
+
+	@Context
+	protected AcceptLanguage contextAcceptLanguage;
+
+	@Context
+	protected Company contextCompany;
+
+	@Context
+	protected UriInfo contextUriInfo;
+
+}
