fc9449b88f99eaa076c27d8fcf045fe39f020ff4
Victor Galan <victor.galan@liferay.com>
2019-11-12 15:14:19 +0100

    Implement put

--- appointments-impl/src/main/java/com/liferay/appointments/internal/resource/v1_0/AppointmentResourceImpl.java
+++ appointments-impl/src/main/java/com/liferay/appointments/internal/resource/v1_0/AppointmentResourceImpl.java
@@ -11,32 +11,25 @@ import com.liferay.dynamic.data.mapping.storage.DDMFormValues;
 import com.liferay.dynamic.data.mapping.storage.Fields;
 import com.liferay.dynamic.data.mapping.util.DDM;
 import com.liferay.journal.model.JournalArticle;
-import com.liferay.journal.service.JournalArticleLocalService;
-import com.liferay.journal.service.JournalArticleLocalServiceUtil;
 import com.liferay.journal.service.JournalArticleService;
 import com.liferay.journal.util.JournalConverter;
+import com.liferay.portal.kernel.exception.PortalException;
 import com.liferay.portal.kernel.service.ServiceContext;
-import com.liferay.portal.kernel.util.LocaleUtil;
 import com.liferay.portal.kernel.util.Portal;
 import com.liferay.portal.vulcan.pagination.Page;
 import com.liferay.portal.vulcan.util.LocalDateTimeUtil;
+import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Reference;
+import org.osgi.service.component.annotations.ServiceScope;
 
 import java.text.SimpleDateFormat;
-
 import java.time.LocalDateTime;
-
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Date;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Locale;
-
-import org.osgi.service.component.annotations.Component;
-import org.osgi.service.component.annotations.Reference;
-import org.osgi.service.component.annotations.ServiceScope;
-
-import javax.validation.constraints.NotNull;
 
 /**
  * @author Javier Gamarra
@@ -70,7 +63,22 @@ public class AppointmentResourceImpl extends BaseAppointmentResourceImpl {
 	}
 
 	@Override
-	public void deleteAppointment(@NotNull Long appointmentId) throws Exception {
+	public Appointment putAppointment(
+		Long appointmentId, Appointment appointment) throws Exception {
+
+		JournalArticle journalArticle = _journalArticleService.getLatestArticle(
+			appointmentId);
+
+		String content = getContent(
+			appointment, journalArticle.getDDMStructure());
+
+		return _toAppointment(
+			_updateJournalArticle(
+				appointment.getTitle(), content, journalArticle));
+	}
+
+	@Override
+	public void deleteAppointment(Long appointmentId) throws Exception {
 		JournalArticle journalArticle = _journalArticleService.getLatestArticle(
 			appointmentId);
 
@@ -86,7 +94,46 @@ public class AppointmentResourceImpl extends BaseAppointmentResourceImpl {
 		JournalArticle journalArticle = _createJournalArticle(
 			siteId, appointment);
 
+		DDMStructure ddmStructure = journalArticle.getDDMStructure();
+
+		String content = getContent(appointment, ddmStructure);
+
+
+
 		return _toAppointment(journalArticle);
+	}
+
+	private JournalArticle _updateJournalArticle(
+			String title, String content, JournalArticle journalArticle) throws PortalException {
+
+		HashMap<Locale, String> titleMap = new HashMap<>();
+
+		LocalDateTime localDateTime = LocalDateTimeUtil.toLocalDateTime(
+			journalArticle.getDisplayDate());
+
+		titleMap.put(
+			contextAcceptLanguage.getPreferredLocale(), title);
+
+		ServiceContext serviceContext = new ServiceContext();
+
+		serviceContext.setScopeGroupId(journalArticle.getGroupId());
+
+		return _journalArticleService.updateArticle(
+			journalArticle.getGroupId(), journalArticle.getFolderId(),
+			journalArticle.getArticleId(), journalArticle.getVersion(),
+			titleMap,
+			null,
+			journalArticle.getFriendlyURLMap(),
+			content,
+			journalArticle.getDDMStructureKey(),
+			null,
+			journalArticle.getLayoutUuid(),
+			localDateTime.getMonthValue() - 1,
+			localDateTime.getDayOfMonth(), localDateTime.getYear(),
+			localDateTime.getHour(), localDateTime.getMinute(), 0, 0, 0, 0,
+			0, true, 0, 0, 0, 0, 0, true, true, false, null, null, null,
+			null,
+			serviceContext);
 	}
 
 	private JournalArticle _createJournalArticle(
@@ -97,6 +144,30 @@ public class AppointmentResourceImpl extends BaseAppointmentResourceImpl {
 			siteId, _portal.getClassNameId(JournalArticle.class),
 			"BASIC-WEB-CONTENT", true);
 
+		String content = getContent(appointment, ddmStructure);
+
+		LocalDateTime localDateTime = LocalDateTimeUtil.toLocalDateTime(
+			new Date());
+
+		HashMap<Locale, String> titleMap = new HashMap<>();
+
+		titleMap.put(
+			contextAcceptLanguage.getPreferredLocale(), appointment.getTitle());
+
+		ServiceContext serviceContext = new ServiceContext();
+
+		serviceContext.setScopeGroupId(siteId);
+
+		return _journalArticleService.addArticle(
+			siteId, 0, 0, 0, null, true, titleMap, null, content,
+			ddmStructure.getStructureKey(), null, null,
+			localDateTime.getMonthValue() - 1, localDateTime.getDayOfMonth(),
+			localDateTime.getYear(), localDateTime.getHour(),
+			localDateTime.getMinute(), 0, 0, 0, 0, 0, true, 0, 0, 0, 0, 0, true,
+			true, null, serviceContext);
+	}
+
+	private String getContent(Appointment appointment, DDMStructure ddmStructure) throws Exception {
 		SimpleDateFormat simpleDateFormat = getDateFormat();
 
 		DDMForm ddmForm = ddmStructure.getDDMForm();
@@ -121,26 +192,7 @@ public class AppointmentResourceImpl extends BaseAppointmentResourceImpl {
 		Fields fields = _ddm.getFields(
 			ddmStructure.getStructureId(), ddmFormValues);
 
-		LocalDateTime localDateTime = LocalDateTimeUtil.toLocalDateTime(
-			new Date());
-		String content = _journalConverter.getContent(ddmStructure, fields);
-
-		HashMap<Locale, String> titleMap = new HashMap<>();
-
-		titleMap.put(
-			contextAcceptLanguage.getPreferredLocale(), appointment.getTitle());
-
-		ServiceContext serviceContext = new ServiceContext();
-
-		serviceContext.setScopeGroupId(siteId);
-
-		return _journalArticleService.addArticle(
-			siteId, 0, 0, 0, null, true, titleMap, null, content,
-			ddmStructure.getStructureKey(), null, null,
-			localDateTime.getMonthValue() - 1, localDateTime.getDayOfMonth(),
-			localDateTime.getYear(), localDateTime.getHour(),
-			localDateTime.getMinute(), 0, 0, 0, 0, 0, true, 0, 0, 0, 0, 0, true,
-			true, null, serviceContext);
+		return _journalConverter.getContent(ddmStructure, fields);
 	}
 
 	private Appointment _toAppointment(JournalArticle journalArticle)
