03c6380f8740ff9478a2d86292f192d0dba7de8a
Victor Galan <victor.galan@liferay.com>
2019-11-26 11:11:42 +0000

    01-listar-citas

---
+++ appointments-api/src/main/java/com/liferay/appointments/resource/v1_0/AppointmentResource.java
@@ -0,0 +1,52 @@
+package com.liferay.appointments.resource.v1_0;
+
+import com.liferay.appointments.dto.v1_0.Appointment;
+import com.liferay.portal.kernel.model.Company;
+import com.liferay.portal.kernel.model.User;
+import com.liferay.portal.vulcan.accept.language.AcceptLanguage;
+import com.liferay.portal.vulcan.pagination.Page;
+
+import javax.annotation.Generated;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import javax.ws.rs.core.UriInfo;
+
+import org.osgi.annotation.versioning.ProviderType;
+
+/**
+ * To access this resource, run:
+ *
+ *     curl -u your@email.com:yourpassword -D - http://localhost:8080/o/appointments/v1.0
+ *
+ * @author Javier Gamarra
+ * @generated
+ */
+@Generated("")
+@ProviderType
+public interface AppointmentResource {
+
+	public Page<Appointment> getSiteAppointmentsPage(Long siteId)
+		throws Exception;
+
+	public default void setContextAcceptLanguage(
+		AcceptLanguage contextAcceptLanguage) {
+	}
+
+	public void setContextCompany(Company contextCompany);
+
+	public default void setContextHttpServletRequest(
+		HttpServletRequest contextHttpServletRequest) {
+	}
+
+	public default void setContextHttpServletResponse(
+		HttpServletResponse contextHttpServletResponse) {
+	}
+
+	public default void setContextUriInfo(UriInfo contextUriInfo) {
+	}
+
+	public void setContextUser(User contextUser);
+
+}
